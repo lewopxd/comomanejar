6ecb2fb1fc13bbd9b78e7f66d7233c60f312a27c
Lewop xd <lewopxd.1@gmail.com>
2021-05-21 21:57:05 -0500

    V3.1 estable

--- myplayer-script.js
+++ myplayer-script.js
@@ -16,13 +16,15 @@ var count = 0;
 var isFirstTime = true;
 var isFirstClick = true;
 
-var isFirstTimetoHide =true;
+var isFirstTimetoHide = true;
 
 var timer = 3000;
 
-var showingElements=false;
+var showingElements = false;
 
 var durationVideo;
+
+var userIsMovingProgress = false;
 
 /* ---------->  [ PLAYER ] <---------   */
 
@@ -55,7 +57,7 @@ player.on('pause', function () {
 });
 
 player.on('bufferstart', function () {
-   // console.log('start buffer the video!');
+    // console.log('start buffer the video!');
     hideElementsContaineronPlay();
 });
 
@@ -74,7 +76,7 @@ player.on('bufferend', function () {
 });
 
 player.on('resize', function (e) {
-   // console.log(e);
+    // console.log(e);
     videoW = e.videoWidth;
     videoH = e.videoHeight;
     console.log("Player.on resize: Video dimensions: " + videoW + " x " + videoH);
@@ -85,76 +87,80 @@ player.on('qualitychange', function (e) {
     // console.log(e);
 });
 player.on('progress', function (getAll) {
-   
-    var percentage = (getAll.percent*100)+"%"
+
+    var percentage = (getAll.percent * 100) + "%"
     var pp = document.getElementById("progressBuffered");
     pp.style.width = percentage;
-   // console.log(percentage);
-     
+    // console.log(percentage);
+
 });
 player.on('timeupdate', function (getAll) {
     currentPos = getAll.seconds; //get currentime
     vdoEndTym = getAll.duration; //get video duration
     percentage = (getAll.percent * 100) + "%";
     seconds = getAll.seconds;
-   
- 
+
+
 
     var ctn = document.getElementById("time-counter");
-    ctn.innerHTML=format(seconds)+" / "+format(vdoEndTym);
+    ctn.innerHTML = format(seconds) + " / " + format(vdoEndTym);
     var pp = document.getElementById("progressPlayed");
-   // pp.style.width = percentage;
+    // pp.style.width = percentage;
 
-    document.getElementById("customRange1").value = ""+(getAll.percent * 100);
-     setCustomRangeColor(getAll.percent*100);   
+    document.getElementById("customRange1").value = "" + (getAll.percent * 100);
+    setCustomRangeColor(getAll.percent * 100);
 
 });
 
 
 
-    player.getDuration().then(function(dur) {
-        // `duration` indicates the duration of the video in seconds
-        durationVideo = dur;
-        
-      }) ;
+player.getDuration().then(function (dur) {
+    // `duration` indicates the duration of the video in seconds
+    durationVideo = dur;
 
-  
+});
+
+
 /*   END------------> PLAYER <---------------    */
 
 
 var range = document.getElementById("customRange1");
- 
-var d=0.0;
 
-   range.addEventListener('input', function () {
+var d = 0.0;
 
-       var percent = range.value;
+range.addEventListener('input', function () {
 
-       var newTime=(percent*durationVideo)/100;
-       
-      console.log(percent+ "  -  new val: " +newTime);
-        
-       var pp = document.getElementById("progressPlayed");
-      // pp.style.width = percent+"%";
-        
+    var percent = range.value;
 
-      var value = (this.value-this.min)/(this.max-this.min)*100;
-      setCustomRangeColor(value);
-       player.setCurrentTime(newTime);
+    var newTime = (percent * durationVideo) / 100;
+
+    console.log(percent + "  -  new val: " + newTime);
+
+    var pp = document.getElementById("progressPlayed");
+    // pp.style.width = percent+"%";
 
 
-          }, false);    
+
+    var ctn = document.getElementById("time-counter");
+    ctn.innerHTML = format(newTime) + " / " + format(durationVideo);
+
+    var value = (this.value - this.min) / (this.max - this.min) * 100;
+    setCustomRangeColor(value);
+    player.setCurrentTime(newTime);
 
 
-          function setCustomRangeColor(value){ 
-            var CustomRange = document.getElementById("customRange1");   
-            
-            var color1 = '#ffffff9a';
-            var color2 ='rgba(255, 255, 255, 0.116)';
-            
-            CustomRange.style.background = 'linear-gradient(to right, '+color1+'  0%, '+color1+' ' + value + '%, '+color2+' ' + value + '%, '+color2+' 100%)';
-       
-          }
+}, false);
+
+
+function setCustomRangeColor(value) {
+    var CustomRange = document.getElementById("customRange1");
+
+    var color1 = '#ffffff9a';
+    var color2 = 'rgba(255, 255, 255, 0.116)';
+
+    CustomRange.style.background = 'linear-gradient(to right, ' + color1 + '  0%, ' + color1 + ' ' + value + '%, ' + color2 + ' ' + value + '%, ' + color2 + ' 100%)';
+
+}
 
 /* 
 function getVideoSize(){
@@ -181,11 +187,11 @@ function getVideoSize(){
 function resizeContainerElements() {
 
     console.log("Il do it");
-    
-   var x = document.getElementById("containerElements");
 
-    x.style.height = "" + (videoH/videoW)*100 + "vw";
-    x.style.maxWidth = "" + (videoW/videoH)*100 + "vh";
+    var x = document.getElementById("containerElements");
+
+    x.style.height = "" + (videoH / videoW) * 100 + "vw";
+    x.style.maxWidth = "" + (videoW / videoH) * 100 + "vh";
 
 }
 
@@ -202,7 +208,7 @@ function browserResize() {
 
 }
 
- 
+
 
 
 
@@ -211,7 +217,7 @@ document.getElementById('playpause-button').onclick = function () {
     var vimeoVideo = document.getElementById('vimeo-video');
     if (isFirstClick) {
         isFirstClick = false;
-        vimeoVideo.style.opacity="1";
+        vimeoVideo.style.opacity = "1";
     }
     var element = document.getElementById('containerElements');
 
@@ -220,12 +226,12 @@ document.getElementById('playpause-button').onclick = function () {
 
     var ppbutton = document.getElementById('playpause-button');
     var dateTitle = document.getElementById('dateTitle');
-    
 
 
-       if(showingElements){
 
-       
+    if (showingElements) {
+
+
         if (isPaused) {
 
             hideElementsContaineronPlay();
@@ -235,20 +241,20 @@ document.getElementById('playpause-button').onclick = function () {
             dateTitle.innerHTML = datetit + "&#9658";
 
             vimeoVideo.style.opacity = "1";
-            isFirstTimetoHide =false;
+            isFirstTimetoHide = false;
 
-            timer=0;
+            timer = 0;
         } else {
-             showElementsContaineronPause();
+            showElementsContaineronPause();
             player.pause();
             ppbutton.innerHTML = "PAUSED";
             vimeoVideo.style.opacity = ".5";
             dateTitle.innerHTML = datetit;
-           
+
         }
-       }else{
+    } else {
         onMove();
-       }
+    }
 }
 
 /*
@@ -266,30 +272,30 @@ document.getElementById('containerElements').addEventListener('mouseup', e => {
 });
 */
 var timeout;
-var justone=0;
+var justone = 0;
 
-var firstcX=0;
-var firstcY=0;
+var firstcX = 0;
+var firstcY = 0;
 
-var cX=0;
-var cY=0;
+var cX = 0;
+var cY = 0;
 
 document.getElementById('containerElements').addEventListener('mousemove', e => {
-      
-    cX=e.clientX;
-    cY=e.clientY;
 
-    if(firstcX!=cX || firstcY!=cY ){
-         //console.log(">>>>mousemov: someone is moving the cursor");
-         firstcX=cX;
-         firstcY=cY;
-    }else{
+    cX = e.clientX;
+    cY = e.clientY;
+
+    if (firstcX != cX || firstcY != cY) {
+        //console.log(">>>>mousemov: someone is moving the cursor");
+        firstcX = cX;
+        firstcY = cY;
+    } else {
 
     }
 
-   // console.log(e.clientX); 
-       onMove();
-   
+    // console.log(e.clientX); 
+    onMove();
+
 
 });
 
@@ -297,65 +303,65 @@ document.getElementById('containerElements').addEventListener('mousemove', e => 
 
 
 
-function onMove(){
-var elemntsContainer = document.getElementById("containerElements");
-var ppButton = document.getElementById("playpause-button");
+function onMove() {
+    var elemntsContainer = document.getElementById("containerElements");
+    var ppButton = document.getElementById("playpause-button");
 
- 
-justone++;
-if (justone==1){
-//console.log("fisrt move");
-showElementsContainerOnMove(true);
-timer=3000;
-}
 
-clearTimeout(timeout);
-timeout = setTimeout(function () {
-  
-       //console.log("chao");
-       showElementsContainerOnMove(false);
-       justone=0;
-}, timer);
+    justone++;
+    if (justone == 1) {
+        //console.log("fisrt move");
+        showElementsContainerOnMove(true);
+        timer = 3000;
+    }
+
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+
+        //console.log("chao");
+        showElementsContainerOnMove(false);
+        justone = 0;
+    }, timer);
 }
 
 
 
 function showElementsContainerOnMove(show) {
- 
-    if(isFirstTimetoHide){
- show=true;
-  }
-  
-     
-    if (!isPaused || isFirstClick ) {
+
+    if (isFirstTimetoHide) {
+        show = true;
+    }
+
+
+    if (!isPaused || isFirstClick) {
         if (show) {
-            
+
             showElementsContaineronPause();
 
         } else {
             hideElementsContaineronPlay();
 
         }
-    
-}
+
+    }
 }
 
 function hideElementsContaineronPlay() {
-    
+
     var elemntsContainer = document.getElementById("containerElements");
     var ppButton = document.getElementById("playpause-button");
 
     elemntsContainer.style.opacity = "0";
-    
+
     //document.body.classList.add('block');
     //document.body.click();
 
-    showingElements=false;
+    showingElements = false;
     //console.log("hiden");
 }
 
 function showElementsContaineronPause() {
-   
+
     var elemntsContainer = document.getElementById("containerElements");
     var ppButton = document.getElementById("playpause-button");
 
@@ -363,7 +369,7 @@ function showElementsContaineronPause() {
     //document.body.classList.add('block');
     //document.body.click();
 
-    showingElements=true;
+    showingElements = true;
     //console.log("showed");
 
 }
@@ -372,17 +378,17 @@ function showElementsContaineronPause() {
 
 
 document.getElementById('fullscreen-icon').onclick = function () {
-toggleFullScreen();
+    toggleFullScreen();
 }
 
 function toggleFullScreen() {
-if (!document.fullscreenElement) {
-   openFullscreen();
-} else {
-  if (document.exitFullscreen) {
-   closeFullscreen();
-  }
-}
+    if (!document.fullscreenElement) {
+        openFullscreen();
+    } else {
+        if (document.exitFullscreen) {
+            closeFullscreen();
+        }
+    }
 }
 
 
@@ -393,28 +399,32 @@ var elem = document.documentElement;
 
 /* View in fullscreen */
 function openFullscreen() {
-if (elem.requestFullscreen) {
-elem.requestFullscreen();
-} else if (elem.webkitRequestFullscreen) { /* Safari */
-elem.webkitRequestFullscreen();
-} else if (elem.msRequestFullscreen) { /* IE11 */
-elem.msRequestFullscreen();
-}
-document.getElementById("fullscreen-icon2").style.opacity="1";
-document.getElementById("fullscreen-icon").style.opacity="0";
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+        /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) {
+        /* IE11 */
+        elem.msRequestFullscreen();
+    }
+    document.getElementById("fullscreen-icon2").style.opacity = "1";
+    document.getElementById("fullscreen-icon").style.opacity = "0";
 }
 
 /* Close fullscreen */
 function closeFullscreen() {
-if (document.exitFullscreen) {
-document.exitFullscreen();
-} else if (document.webkitExitFullscreen) { /* Safari */
-document.webkitExitFullscreen();
-} else if (document.msExitFullscreen) { /* IE11 */
-document.msExitFullscreen();
-}
-document.getElementById("fullscreen-icon").style.opacity="1";
-document.getElementById("fullscreen-icon2").style.opacity="0";
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+        /* IE11 */
+        document.msExitFullscreen();
+    }
+    document.getElementById("fullscreen-icon").style.opacity = "1";
+    document.getElementById("fullscreen-icon2").style.opacity = "0";
 }
 
 
@@ -424,19 +434,19 @@ document.getElementById("fullscreen-icon2").style.opacity="0";
 
 
 
-function vhsBlur(timerD){
-document.body.style.color="#eee";
-document.body.style.textShadow="0.06rem 0 0.06rem #ea36af, -0.125rem 0 0.06rem #75fa69";
- document.body.style.animationDuration=timerD;
-document.body.style.animationName="textflicker";
-document.body.style.animationIterationCount="infinite";
-document.body.style.animationDirection="alternate";
-  
+function vhsBlur(timerD) {
+    document.body.style.color = "#eee";
+    document.body.style.textShadow = "0.06rem 0 0.06rem #ea36af, -0.125rem 0 0.06rem #75fa69";
+    document.body.style.animationDuration = timerD;
+    document.body.style.animationName = "textflicker";
+    document.body.style.animationIterationCount = "infinite";
+    document.body.style.animationDirection = "alternate";
+
 }
 
 
 
-function format(time) {   
+function format(time) {
     // Hours, minutes and seconds
     var hrs = ~~(time / 3600);
     var mins = ~~((time % 3600) / 60);
@@ -447,7 +457,7 @@ function format(time) {
     if (hrs > 0) {
         ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
     }
-    ret += "" +  (mins < 10 ? "0":"")+mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + (mins < 10 ? "0" : "") + mins + ":" + (secs < 10 ? "0" : "");
     ret += "" + secs;
 
     return ret;
@@ -460,16 +470,16 @@ function format(time) {
 
 
 window.addEventListener('keydown', e => {
-  console.log(e.code);
-   if(e.code=="Space"){
-        if(isPaused){
+    console.log(e.code);
+    if (e.code == "Space") {
+        if (isPaused) {
             player.play();
             hideElementsContaineronPlay();
-        }else{
+        } else {
             player.pause();
             showElementsContaineronPause();
         }
-   } 
+    }
 
 });
 
